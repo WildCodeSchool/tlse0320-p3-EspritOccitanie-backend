0dc2295cc62409c683f5592bf50c992f2b90ef32
Jérôme HAMILKA <jerome.hamilka@gmail.com>
2020-06-10 10:00:20 +0200

    first route

--- migrations/test/sqls/20200607135158-init-DB-down.sql
+++ migrations/test/sqls/20200607135158-init-DB-down.sql
@@ -1,3 +1,3 @@
 /* Replace with your SQL commands */
 
-DROP TABLE `users`;
+TRUNCATE TABLE `ro_animator`;
--- src/server.js
+++ src/server.js
@@ -8,61 +8,24 @@ const connection = require('../db');
 app.use(express.json());
 app.use(cors());
 
-app.get('/users', (req, res) => {
-  connection.query('SELECT * from users', (error, results) => {
-    res.status(200).json({
-      status: 'success',
-      results,
-    });
-  });
-});
-
-app.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-  try {
-    connection.query('SELECT * from users WHERE `user_id`= ?', id, (error, results) => {
-      if (results.length === 0) {
-        res.status(404).json({
-          status: 'error',
-          errorMessage: 'Not found',
-        });
+app.get('/animators', (req, res) => {
+  connection.query("SELECT * FROM ro_animator", (err, results) => {
+    console.log(results)
+    if (err) {
+      res.status(404).json({ message: 'bad request !' })
+    } else {
+      if (results.length) {
+        res.status(200).json(results[0]);
       } else {
-        res.status(200).json({
-          status: 'success',
-          results: results[0],
-        });
+        res.status(404).json({ error: "Animator not found" });
       }
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      errorMessage: 'Our server encountered an error performing the request',
-    });
-  }
-});
+    }
+  });
+})
 
-app.post('/users', (req, res) => {
-  const { body: formData } = req;
-  try {
-    const query = 'INSERT INTO users SET ?';
-    connection.query(query, formData, (error, results) => {
-      res.status(201).json({
-        status: 'success',
-        userCreated: {
-          user_id: results.insertId,
-          ...formData,
-        },
-      });
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      errorMessage: 'Our server encountered an error performing the request',
-    });
-  }
-});
 const server = app.listen(PORT, () => {
   console.log(`🌍 Server is running on port ${PORT}`);
 });
 
 module.exports = server;
+
