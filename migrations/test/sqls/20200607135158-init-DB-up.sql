012766e9e02ec9d226dcc5652d834c976892ee2e
tzoitzeot <tristan.maciag@icloud.com>
2020-06-10 12:17:33 +0200

    start config

--- migrations/test/sqls/20200607135158-init-DB-up.sql
+++ migrations/test/sqls/20200607135158-init-DB-up.sql
@@ -1,4 +1,11 @@
-
+-- CREATE TABLE `ro_animator` (
+--   `animator_id` int(11) NOT NULL AUTO_INCREMENT,
+--   `animator_firstname` varchar(45) NOT NULL,
+--   `animator_lastname` varchar(45) NOT NULL,
+--   `animator_description` text,
+--   `animator_image` varchar(255) DEFAULT NULL,
+--   PRIMARY KEY (`animator_id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 
 INSERT INTO `ro_animator` VALUES 
