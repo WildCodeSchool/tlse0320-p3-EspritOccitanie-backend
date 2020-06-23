4dd79a7f2e1a24414c87bb25b9f68e4a04a6a2b9
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-06-23 16:02:23 +0200

    mise à jours checkerrors

--- controllers/checkErrors.js
+++ controllers/checkErrors.js
@@ -4,22 +4,22 @@ const { validationResult } = require('express-validator');
 
 class Check {
   static error(req, res, next) {
-    const errorsAnimatorValidation = validationResult(req);
+    const errorsValidation = validationResult(req);
 
-    if (!errorsAnimatorValidation.isEmpty()) {
-    //   console.log(errorsAnimatorValidation.errors);
-      if (errorsAnimatorValidation.errors.find((error) => error.msg === 'value undefined')) {
+    if (!errorsValidation.isEmpty()) {
+    //   console.log(errorsValidation.errors);
+      if (errorsValidation.errors.find((error) => error.msg === 'value undefined')) {
         return res.status(500).json({ error: 'value undefined' });
       }
       if (
-        errorsAnimatorValidation.errors.find((error) => error.msg === 'empty or blank caracteres')
+        errorsValidation.errors.find((error) => error.msg === 'empty or blank caracteres')
       ) {
         return res.status(500).json({ error: 'empty or blank caracteres' });
       }
-      if (errorsAnimatorValidation.errors.find((error) => error.msg === 'is integer')) {
+      if (errorsValidation.errors.find((error) => error.msg === 'is integer')) {
         return res.status(500).json({ error: 'is integer' });
       }
-      if (errorsAnimatorValidation.errors.find((error) => error.msg === 'is not email')) {
+      if (errorsValidation.errors.find((error) => error.msg === 'is not email')) {
         return res.status(500).json({ error: 'is not email' });
       }
     }
