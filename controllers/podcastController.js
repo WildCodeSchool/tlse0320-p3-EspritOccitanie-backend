5374f3eda9257cba6251d5ecb5839f7e7012aae3
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-06-29 17:45:13 +0200

    route add animator to podcast

--- controllers/podcastController.js
+++ controllers/podcastController.js
@@ -4,72 +4,79 @@
 const { PodcastModel } = require('../models/podcastModel');
 
 class PodcastController {
-    static postPodcast(req, res) {
-        PodcastModel.postPodcast(req, (err, results) => {
-            if (err) {
-                return res.status(500).json({ error: `${err}`, data: 'refq' });
-            }
+  static addAnimator(req, res) {
+    PodcastModel.addAnimator(req, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: `${err}`, data: 'refq' });
+      }
 
-            return res
-                .status(200)
-                .json({ message: 'OK', dataSend: req.body, affectedRows: results.affectedRows });
-        });
-    }
+      return res
+        .status(200)
+        .json({ message: 'OK', dataSend: req.body, affectedRows: results.affectedRows });
+    });
+  }
 
-    static getAllPodcasts(req, res) {
-        PodcastModel.getAllPodcasts(req, (err, results) => {
-            if (err) {
-                return res.status(500).json({ error: `${err}` });
-            }
-            return res
-                .status(200)
-                .json(results);
-        });
-    }
+  static postPodcast(req, res) {
+    PodcastModel.postPodcast(req, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: `${err}`, data: 'refq' });
+      }
+      req.podcast_id = results.insertId;
+      const ro_podcast_podcast_id = req.podcast_id;
+      PodcastModel.addAnimator(req, ro_podcast_podcast_id, (err1, result1) => {
+        if (err1) {
+          return res.status(500).json({ error: `${err1}`, data: 'refq' });
+        }
+        console.log('addAnimator', result1);
+        return res.status(200).json(result1);
+      });
+    });
+  }
 
-    static getOnePodcast(req, res) {
-        PodcastModel.getOnePodcast(req, (err, results) => {
-            if (err) {
-                return res.status(500).json({ error: `${err}` });
-            }
-            return res
-                .status(200)
-                .json(results[0]);
-        });
-    }
+  static getAllPodcasts(req, res) {
+    PodcastModel.getAllPodcasts(req, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: `${err}` });
+      }
+      return res.status(200).json(results);
+    });
+  }
 
-    static get4LatestPodcasts(req, res) {
-        PodcastModel.get4LatestPodcasts(req, (err, results) => {
-            if (err) {
-                return res.status(500).json({ error: `${err}` });
-            }
-            return res
-                .status(200)
-                .json(results);
-        });
-    }
+  static getOnePodcast(req, res) {
+    PodcastModel.getOnePodcast(req, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: `${err}` });
+      }
+      return res.status(200).json(results[0]);
+    });
+  }
 
-    static delPodcast(req, res) {
-        PodcastModel.delPodcast(req, (err) => {
-            if (err) {
-                return res.status(500).json({ error: `${err}` });
-            }
-            return res
-                .status(200)
-                .json({ message: 'field deleted' });
-        });
-    }
+  static get4LatestPodcasts(req, res) {
+    PodcastModel.get4LatestPodcasts(req, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: `${err}` });
+      }
+      return res.status(200).json(results);
+    });
+  }
 
-    static putPodcast(req, res) {
-        PodcastModel.putPodcast(req, (err, results) => {
-            if (err) {
-                return res.status(500).json({ error: `${err}` });
-            }
-            return res
-                .status(200)
-                .json(results[0]);
-        });
-    }
+  static delPodcast(req, res) {
+    PodcastModel.delPodcast(req, (err) => {
+      if (err) {
+        return res.status(500).json({ error: `${err}` });
+      }
+      return res.status(200).json({ message: 'field deleted' });
+    });
+  }
+
+  static putPodcast(req, res) {
+    PodcastModel.putPodcast(req, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: `${err}` });
+      }
+      return res.status(200).json(results[0]);
+    });
+  }
 }
 
 module.exports = { PodcastController };
--- models/podcastModel.js
+++ models/podcastModel.js
@@ -2,45 +2,96 @@
 const connection = require('../db');
 
 class PodcastModel {
-    static postPodcast(req, callback) {
-        connection.query('INSERT INTO ro_podcast SET ?', [req.body], (err, results) => {
-            callback(err, results);
-        });
-    }
+  static addAnimator(req, ro_podcast_podcast_id, callback) {
+    const { ro_animator_animator_id } = req.body;
 
-    static getAllPodcasts(req, callback) {
-        connection.query('SELECT * FROM ro_podcast', (err, results) => {
-            callback(err, results);
-        });
-    }
+    const podcastAnimator = {
+        ro_animator_animator_id,
+        ro_podcast_podcast_id, 
+      };
 
-    static get4LatestPodcasts(req, callback) {
-        connection.query('SELECT *, DATE_FORMAT(podcast_creation_date, "%d/%m/%Y") FROM ro_podcast order by podcast_creation_date desc limit 4', (err, results) => {
-            callback(err, results);
-        });
-    }
+    connection.query(
+      'INSERT INTO ro_podcast_has_ro_animator SET ?',
+      [podcastAnimator],
+      (err1, result1) => {
 
-    static putPodcast(req, callback) {
-        connection.query('UPDATE ro_podcast SET ? WHERE podcast_id = ?', [req.body, req.params.id], (err, results) => {
-            callback(err, results);
-        });
-    }
 
-    static delPodcast(req, callback) {
-        connection.query('DELETE FROM ro_podcast WHERE podcast_id = ?', req.params.id, (err, results) => {
-            callback(err, results);
-        });
-    }
+        callback(err1, result1);
+      },
+    );
+  }
 
-    static getOnePodcast(id, cb) {
-        connection.query(
-            'SELECT * FROM ro_podcast WHERE id = ?',
-            id,
-            (err, results, fields) => {
-                cb(err, results, fields);
-            },
-        );
-    }
+  static postPodcast(req, callback) {
+    const {
+      podcast_title,
+      podcast_duration,
+      podcast_description,
+      podcast_image,
+      podcast_mp3,
+      podcast_creation_date,
+      ro_category_category_id,
+      ro_program_program_id,
+      ro_animator_animator_id,
+    } = req.body;
+
+    const dataPocast = {
+      podcast_title,
+      podcast_duration,
+      podcast_description,
+      podcast_image,
+      podcast_mp3,
+      podcast_creation_date,
+      ro_category_category_id,
+      ro_program_program_id,
+    };
+
+    console.log(ro_animator_animator_id);
+
+    connection.query('INSERT INTO ro_podcast SET ?', [dataPocast], (err, results) => {
+      callback(err, results);
+    });
+  }
+
+  static getAllPodcasts(req, callback) {
+    connection.query('SELECT * FROM ro_podcast', (err, results) => {
+      callback(err, results);
+    });
+  }
+
+  static get4LatestPodcasts(req, callback) {
+    connection.query(
+      'SELECT *, DATE_FORMAT(podcast_creation_date, "%d/%m/%Y") FROM ro_podcast order by podcast_creation_date desc limit 4',
+      (err, results) => {
+        callback(err, results);
+      },
+    );
+  }
+
+  static putPodcast(req, callback) {
+    connection.query(
+      'UPDATE ro_podcast SET ? WHERE podcast_id = ?',
+      [req.body, req.params.id],
+      (err, results) => {
+        callback(err, results);
+      },
+    );
+  }
+
+  static delPodcast(req, callback) {
+    connection.query(
+      'DELETE FROM ro_podcast WHERE podcast_id = ?',
+      req.params.id,
+      (err, results) => {
+        callback(err, results);
+      },
+    );
+  }
+
+  static getOnePodcast(id, cb) {
+    connection.query('SELECT * FROM ro_podcast WHERE id = ?', id, (err, results, fields) => {
+      cb(err, results, fields);
+    });
+  }
 }
 
 module.exports = { PodcastModel };
