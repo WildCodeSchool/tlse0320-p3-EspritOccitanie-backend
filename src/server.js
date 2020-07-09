2c7fe1f45126f152e84355de535dcc7eca3636c9
jerome <jerome.hamilka@gmail.com>
2020-07-09 16:56:10 +0200

    form-mail-back

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000);
@@ -10,6 +11,36 @@ app.use(cors());
 const routes = require('./routes');
 
 app.use('/', routes);
+
+app.post('/Contact', function (req, res, next) {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'jerome.hamilka@gmail.com',
+      pass: 'password'
+    }
+  }
+  );
+  var mailOptions = {
+    from: req.body.sender,
+    to: req.body.destination,
+    subject: req.body.subject,
+    text: req.body.message,
+    html: '<b>' + req.body.message + '</b>'
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
+
+  transporter.close();
+});
+
+app.use(function (req, res) {
+  res.sendStatus(404);
+});
 
 const server = app.listen(PORT, () => {
   console.log(`🌍 Server is running on port ${PORT} `);
