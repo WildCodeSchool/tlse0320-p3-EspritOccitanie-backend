564246af357bca8f094296086191df1a90d7fcfe
jerome <jerome.hamilka@gmail.com>
2020-07-15 15:00:43 +0200

    dump db down & up

--- migrations/dev/sqls/20200607135158-init-DB-down.sql
+++ migrations/dev/sqls/20200607135158-init-DB-down.sql
@@ -1,3 +1,18 @@
 /* Replace with your SQL commands */
+SET FOREIGN_KEY_CHECKS = 0; 
 
-DROP TABLE `users`;
+DROP TABLE IF EXISTS `ro_admin`;
+DROP TABLE IF EXISTS `ro_animator_has_ro_program`;
+DROP TABLE IF EXISTS `ro_animator`;
+DROP TABLE IF EXISTS `ro_category`;
+DROP TABLE IF EXISTS `ro_planning`;
+DROP TABLE IF EXISTS `ro_program`;
+DROP TABLE IF EXISTS `ro_podcast`;
+DROP TABLE IF EXISTS `ro_tag`;
+DROP TABLE IF EXISTS `ro_planning_has_ro_podcast`;
+DROP TABLE IF EXISTS `ro_podcast_has_ro_animator`;
+DROP TABLE IF EXISTS `ro_podcast_has_ro_tag`;
+DROP TABLE IF EXISTS `ro_program_has_ro_planning`;
+DROP TABLE IF EXISTS `ro_tag_has_ro_program`;
+
+SET FOREIGN_KEY_CHECKS = 1; 
